73fa6a0328237f7b54829e9710054b59a72effe5
Barry Luijbregts <b.luijbregts@waardedoorit.nl>
2023-01-02 17:04:46 +0000

    Storage Account storeg image

---
+++ 3. Store Data in Azure/1. Application connects with Azure Storage/WebAppWithStorage/Controllers/StorageController.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppWithStorage.Controllers
+{
+    public class StorageController : Controller
+    {
+        private readonly ILogger<StorageController> _logger;
+
+        public StorageController(ILogger<StorageController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult AzureStorage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AzureStorage(IFormFile fimage)
+        {
+            var filename = fimage.FileName;
+            var fileUrl = "";
+            BlobContainerClient container = new BlobContainerClient("DefaultEndpointsProtocol=https;AccountName=<Storage Account name>;AccountKey=<Storage Account Key>",
+            "<Storage Blob Container Name>");
+            try
+            {
+                BlobClient blob = container.GetBlobClient(filename);
+                using (Stream stream = fimage.OpenReadStream())
+                {
+                    blob.Upload(stream);
+                }
+                fileUrl = blob.Uri.AbsoluteUri;
+            }
+            catch (Exception ex) { }
+
+            return View();
+        }
+
+    }
+}
